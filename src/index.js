bbbb5008c71c7b006586e3c273203778eb0c6ac1
Delarta Tok Adin <delarta.tok@gmail.com>
2019-05-25 17:56:02 +0700

    finishing upvote and downvote function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'uikit/dist/css/uikit.min.css'
+import 'uikit/dist/js/uikit'
+import './assets/css/themify-icons.css'
+import './assets/css/_main.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/pages/HomePage.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+class HomePage extends Component {
+  render() {
+    var posts = this.props.posts.sort((a,b) => (a.vote < b.vote) ?  1 : (a.vote > b.vote) ? -1 : 0 );
+    return (
+      <div className="uk-container">
+        {posts.map(item => (
+          <div
+          key={item.id}
+            className="uk-card uk-card-default uk-grid-collapse topics"
+            uk-grid="true"
+          >
+            <div className="uk-card-media-left uk-cover-container uk-width-auto@s vote">
+              <div onClick={() => this.props.upvote(item.id)}><i className="ti-arrow-up"></i></div>
+              <div>
+                {item.vote}
+              </div>
+              <div onClick={() => this.props.downvote(item.id)}><i className="ti-arrow-down"></i></div>
+            </div>
+            <div className="uk-width-expand@s">
+              <div className="uk-card-body">
+                <h3 className="uk-card-title">
+                  {item.title}
+                </h3>
+                <div>
+                  {item.body}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default withRouter(HomePage);
